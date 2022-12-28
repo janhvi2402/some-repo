a96620618b30c53d764ffce03a21023ef02abd31
janhvi2402 <76250810+janhvi2402@users.noreply.github.com>
2022-12-28 12:01:53 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Indian Institute of Information Technology, Surat
+
+## Assignment Starter Files - Python
+
+- main.py(for the code of the assignment)
+
+### How to run the code
+
+#### Requirements
+
+- Python 3.6 or above
+
+#### Steps
+
+- `python main.py`
+
+#### Instructions
+
+- You are free to use your own files and folder structure, subject to the condition that the finall call should be taking place in main.py.
---
+++ main.py
@@ -0,0 +1,33 @@
+
+"""
+Below statements are for File I/O
+Uncomment the next multi line comment to use File I/O.
+IMPORTANT: You should not submit the code with File I/O statements. They are only for local testing.
+"""
+
+"""
+# import sys
+Read the input from input.txt
+sys.stdin = open('input.txt', 'r')
+
+Write the output to output.txt
+sys.stdout = open('output.txt', 'w')
+"""
+
+"""
+End of File I/O statements
+"""
+
+
+def main():
+    """
+    Write your code here
+    You can make use of classes, and functions
+    """
+    
+    return
+
+if __name__ == '__main__':
+    # Read the input from stdin
+    main()
+    exit(0)
